acfcd952e5056eb719060d69d82f3fa7fe6e45cf
geyujie <961211746@qq.com>
2021-10-27 10:29:41 +0800

    header router

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,15 +1,12 @@
 let currentLanguage = 'EN'
-const HeaderHTML = `<div class="app-header">
-    <a href="/">
-        <div class="app-header-title">
-            <img class="title-icon" src="images/logo.png" alt="" />
-            <p class="title-label">${currentLanguage === 'CN' ? "葛宇杰的个人网站" : "Ge Yujie's personal website"}</p>
-        </div>
-    </a>
+const HeaderHTML =
+    `<div class="app-header">
+    <div class="app-header-title" id="app-title">
+        <img class="title-icon" src="images/logo.png" alt="" />
+        <p class="title-label">${currentLanguage === 'CN' ? "葛宇杰的个人网站" : "Ge Yujie's personal website"}</p>
+    </div>
     <div class="app-header-directory">
-        <a href="/home">
-            <div class="nav-item">${currentLanguage === 'CN' ? '主页' : 'Home'}</div>
-        </a>
+        <div class="nav-item" id="app-home">${currentLanguage === 'CN' ? '主页' : 'Home'}</div>
         <div class="nav-item">
             <select id="changeLanguage" disabled>
                 <option value="EN">${currentLanguage === 'CN' ? '英文' : 'english'}</option>
@@ -21,8 +18,34 @@ const HeaderHTML = `<div class="app-header">
 const RootDom = document.getElementById("root")
 // RootDom.appendChild(HeaderHTML)
 RootDom.innerHTML = HeaderHTML
-document.getElementById("changeLanguage").addEventListener('change',changeLanguage)
-function changeLanguage(e){
+document.getElementById("changeLanguage").addEventListener('change', changeLanguage)
+function changeLanguage(e) {
     currentLanguage = e.target.value
-    console.log('当前语言：',currentLanguage)
+    console.log('当前语言：', currentLanguage)
+}
+document.getElementById("app-title").addEventListener('click', changeRouter)
+function changeRouter() {
+    const { pathname } = window.location;
+    switch (pathname) {
+        case '/':
+            break;
+        case '/home':
+            window.location.replace('/');
+            break;
+        default:
+            break;
+    }
+}
+document.getElementById("app-home").addEventListener('click', isGoHome)
+function isGoHome(){
+    const { pathname } = window.location;
+    switch (pathname) {
+        case '/':
+            window.location.replace('/home');
+            break;
+        case '/home':
+            break;
+        default:
+            break;
+    }
 }
